169a2c644cb1401fc2c4afd90de347c01f718dd9
macbook air <macbookair@MACs-MacBook-Air.local>
2025-07-27 07:17:52 +0100

    minor update to app.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import 'react-native-gesture-handler'; // 👈 Make sure this is at the top
+import { GestureHandlerRootView } from "react-native-gesture-handler"; // 👈 Import this
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./Screens/HomeScreen";
@@ -8,11 +9,21 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="WeatherDetailScreen" component={WeatherDetailScreen} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}> 
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="WeatherDetailScreen"
+            component={WeatherDetailScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
